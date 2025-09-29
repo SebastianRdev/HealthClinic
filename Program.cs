499aac024d7d7eb129b3bde14e44e8e9803625f1
SebastianRdev <sebastianrdev@outlook.com>
2025-09-29 12:17:11 -0500

    feat: Added Utils folder and created methods to display the initial menu

--- Program.cs
+++ Program.cs
@@ -2,14 +2,48 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        show_menu();
+        menu();
     }
 
-    public static void menu()
+    public static void show_menu()
     {
         Console.WriteLine("1. Register Customer");
         Console.WriteLine("2. View Customers");
         Console.WriteLine("3. Search Customers");
         Console.WriteLine("4. Exit");
+    }
+
+    public static void menu()
+    {
+        while (true)
+        {
+            try {
+                Console.Write("Enter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice){
+                    case 1:
+                        RegisterCustomer();
+                        break;
+                    case 2:
+                        ViewCustomers();
+                        break;
+                    case 3:
+                        SearchCustomers();
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        show_menu();
+                        menu();
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
     }
 }
--- models/Customer.cs
+++ models/Customer.cs
@@ -6,4 +6,42 @@ public class Customer
     public string? Name { get; set; }
     public int Ages { get; set; }
     public List<Pet> Pets { get; set; } = new List<Pet>();
+
+    // public Customer(string name, int ages)
+    // {
+    //     Id = Guid.NewGuid();
+    //     Name = name;
+    //     Ages = ages;
+    // }
+
+    public static void MainRegisterCustomer()
+    {
+        RegisterCustomerMenu();
+        Console.WriteLine("Customer registered successfully");
+
+    }
+
+    public static void RegisterCustomerMenu()
+    {
+        while (true)
+        {
+            Console.Write("Enter customer name: ");
+            string name = Console.ReadLine()!;
+            break;
+        }
+        
+        Console.Write("Enter customer ages: ");
+        int ages = Convert.ToInt32(Console.ReadLine());
+        RegisterCustomer(name, ages);
+    }
+
+    public static void RegisterCustomer(string name, int ages)
+    {
+        Customer newCustomer = new Customer
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            Ages = ages
+        };
+    }
 }
